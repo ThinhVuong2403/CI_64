4f9736694cd4a57114bbe3350467e66bacc1aa9a
ThinhVuong2403 <vuongquocthinh24031999@gmail.com>
2021-07-28 08:54:15 +0700

    add lesson

---
+++ sesion_1_es6/1_variable.js
@@ -0,0 +1,10 @@
+// anonymous function as variable
+
+const sub = function (a, b){
+    return a - b;
+}
+
+console.log('sub', sub(2, 1));
+
+const pow = (a, b) => a ** b;
+console.log('pow', pow(2, 0));
